8df3345c83c5e486aa2d7d418951dc5076c52c29
Vikrant Singh <vikrantpbh@gmail.com>
2022-01-13 23:19:22 +0530

    Grid layout

--- growth-chart/src/myComponents/growthChartTool/ChartGridTable.js
+++ growth-chart/src/myComponents/growthChartTool/ChartGridTable.js
@@ -1,9 +1,10 @@
 import React from 'react'
-
+import '../growthChartTool/ChartGridTable.css'
 const ChartGridTable = () => {
     return (
         <div className='chartGridContainer'>
-            
+            <div className='gridCell'>Year</div>
+            <div className='gridCehll'></div>
         </div>
     )
 }
